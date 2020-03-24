ee24294d52b7334dcd3ecb96e0f5b3877254a3e5
nicksimmons53 <nicksimmons53@gmail.com>
2020-03-23 22:12:45 -0500

    Initialize Repo

---
+++ src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <meta http-equiv="refresh" content="5; url=http://www.besgroups.com/home/"/>
+    <img src="../assets/BNA_Logo.jpg"/>
+    <header>
+      <h1>Welcome to Biosure North America</h1>
+      <div class="text">
+        <p>Our website is currently under construction. In the meantime, please wait for redirection to our technical website.</p>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home'
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1 {
+    color: #00A3E0;
+  }
+  img {
+    width: 300px;
+    height: 100px;
+  }
+</style>
